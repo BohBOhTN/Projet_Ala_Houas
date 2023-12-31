87086a54dc4ad957089d5508e01c98720b1b4906
BohBOhTN <90150981+BohBOhTN@users.noreply.github.com>
2023-12-31 08:31:12 +0100

    making the cleaning process faster

--- Scraping/script.py
+++ Scraping/script.py
@@ -4,27 +4,40 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 clean_file_path = './clean/clean_data.txt'
-file_path = './data/data.txt'
 
 def get_text_from_element(driver, element):
     driver.execute_script("arguments[0].scrollIntoView();", element)
     return element.text
+
+def clean_text(text):
+    cleaned_text = text.replace('\n', ' ').replace('\r', '')  # Example: Removing line breaks and carriage returns
+    # You can add more cleaning operations as needed
+    return cleaned_text
+
+def remove_first_n_lines(text, n):
+    lines = text.split('\n')
+    cleaned_lines = lines[n:]
+    cleaned_text = '\n'.join(cleaned_lines)
+    return cleaned_text
 
 def accepting():
     driver = webdriver.Chrome()
     driver.get("https://bourse.societegenerale.fr/services/newsletters/bourse-matin")
 
     try:
+        # Click on the "Tout sélectionner" button
         button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, "(//button[normalize-space()='Tout sélectionner'])[1]"))
         )
         button.click()
 
+        # Wait for the iframe to be present and switch to it
         iframe = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//div[@id='editorial-3302']//p//iframe"))
         )
         driver.switch_to.frame(iframe)
 
+        # Find and click the "Lire la suite" link
         clot_wall_st = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//div[@class='grid_div r3d_l r2d_l']"))
         )
@@ -32,13 +45,21 @@ def accepting():
         lire_la_suite = clot_wall_st.find_element(By.XPATH, "//a[@class='bouton_suite']")
         lire_la_suite.click()
 
+        # Get text from the designated element
         text_element = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.XPATH, "//div[@class='zText']"))
         )
         text = get_text_from_element(driver, text_element)
 
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(text)
+        # Remove the first 7 lines from the text
+        text = remove_first_n_lines(text, 7)
+
+        # Clean the extracted text
+        cleaned_text = clean_text(text)
+
+        # Write the cleaned text to a file
+        with open(clean_file_path, 'w', encoding='utf-8') as file:
+            file.write(cleaned_text)
 
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -46,14 +67,5 @@ def accepting():
     finally:
         driver.quit()
 
-def remove_first_n_lines(file_path, clean_file_path, n):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-
-    lines = lines[n:]
-
-    with open(clean_file_path, 'w', encoding='utf-8') as file:
-        file.writelines(lines)
-
+# Run the function to extract text, clean it, and save it directly
 accepting()
-remove_first_n_lines(file_path, clean_file_path, 7)
