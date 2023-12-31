f5725de5d014888c9d20e40ceb6b6c99068a2e3a
BohBOhTN <90150981+BohBOhTN@users.noreply.github.com>
2023-12-31 08:14:11 +0100

    Using WebDriverWait instead of Time.sleep

--- Scraping/script.py
+++ Scraping/script.py
@@ -1,29 +1,64 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import time
-driver = webdriver.Chrome()
-driver.get("https://bourse.societegenerale.fr/services/newsletters/bourse-matin")
-time.sleep(2)
-driver.find_element(By.XPATH,"(//button[normalize-space()='Tout sélectionner'])[1]").click()
-time.sleep(3)
-big_container = driver.find_element(By.XPATH,"//div[@id='editorial-3302']//p//iframe")
-driver.switch_to.frame(big_container)
-clot_wall_st = driver.find_element(By.XPATH,"//div[@class='grid_div r3d_l r2d_l']")
-clot_wall_st.find_element(By.XPATH,"//a[@class='bouton_suite']").click()
-text = clot_wall_st.find_element(By.XPATH,"//div[@class='zText']").text
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 clean_file_path = './clean/clean_data.txt'
 file_path = './data/data.txt'
-with open(file_path, 'w', encoding='utf-8') as file:
-    file.write(text)
-driver.quit()
 
-with open(file_path, 'r', encoding='utf-8') as file:
-    lines = file.readlines()
+def get_text_from_element(driver, element):
+    driver.execute_script("arguments[0].scrollIntoView();", element)
+    return element.text
 
-# Remove the first 7 lines
-lines = lines[7:]
+def accepting():
+    driver = webdriver.Chrome()
+    driver.get("https://bourse.societegenerale.fr/services/newsletters/bourse-matin")
 
-# Write the modified content back to the file
-with open(clean_file_path, 'w', encoding='utf-8') as file:
-    file.writelines(lines)
+    try:
+        button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "(//button[normalize-space()='Tout sélectionner'])[1]"))
+        )
+        button.click()
+
+        iframe = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@id='editorial-3302']//p//iframe"))
+        )
+        driver.switch_to.frame(iframe)
+
+        clot_wall_st = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@class='grid_div r3d_l r2d_l']"))
+        )
+        driver.execute_script("arguments[0].scrollIntoView();", clot_wall_st)
+        lire_la_suite = clot_wall_st.find_element(By.XPATH, "//a[@class='bouton_suite']")
+        lire_la_suite.click()
+
+        text_element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//div[@class='zText']"))
+        )
+        text = get_text_from_element(driver, text_element)
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(text)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
+    finally:
+        driver.quit()
+
+def remove_first_n_lines(file_path, clean_file_path, n):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    # Remove the first n lines
+    lines = lines[n:]
+
+    # Write the modified content back to the file
+    with open(clean_file_path, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
+
+# Run the function to extract text
+accepting()
+
+# Remove the first 7 lines from the extracted text file
+remove_first_n_lines(file_path, clean_file_path, 7)
